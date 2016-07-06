8490aa7e178752f7abdf2422aec7d90fbb96b5d7
Bernat Rafales <bernatrafales@notonthehighstreet.com>
2016-07-06 17:54:07 +0100

    Extract method into method object

--- toy_example.rb
+++ toy_example.rb
@@ -1,6 +1,6 @@
 class Checkout
   def complete_checkout
-    calculate_totals
+    TotalsCalculator.new(self).calculate
 
     if pay
       post_completed_actions
@@ -9,24 +9,10 @@ class Checkout
     end
   end
 
-  def calculate_totals
-    self.total = self.orders.map{|o| o.products.map(&:price).reduce(&:+)}.reduce(&:+)
-    calculate_amounts_to_pay
-  end
-
   def post_completed_actions
     self.state = "completed"
     send_completed_email
     MONITOR.increment(:checkout_completed)
-  end
-
-  def calculate_amounts_to_pay
-    self.card_amount = self.total
-    self.user_funds_amount = 0
-    if self.user.has_customer_funds?
-      self.card_amount = self.total - user.customer_funds
-      self.user_funds_amount = user.customer_funds
-    end
   end
 
   def send_completed_email
@@ -36,5 +22,26 @@ class Checkout
 
   def pay
     authorize_money_from_bank(self.card_amount) && debit_funds_from_user(self.user_funds_amount)
+  end
+end
+
+class TotalsCalculator
+
+  def initialize(checkout)
+    @checkout = checkout
+  end
+
+  def calculate
+    @checkout.total = @checkout.orders.map{|o| o.products.map(&:price).reduce(&:+)}.reduce(&:+)
+    calculate_amounts_to_pay
+  end
+
+  def calculate_amounts_to_pay
+    @checkout.card_amount = @checkout.total
+    @checkout.user_funds_amount = 0
+    if @checkout.user.has_customer_funds?
+      @checkout.card_amount = @checkout.total - user.customer_funds
+      @checkout.user_funds_amount = user.customer_funds
+    end
   end
 end
