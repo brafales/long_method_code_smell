61a0a3cb6b6d6b23ace9506553382eecd9cab10f
Bernat Rafales <bernatrafales@notonthehighstreet.com>
2016-07-06 17:14:39 +0100

    Extract calculate_totals

--- toy_example.rb
+++ toy_example.rb
@@ -1,13 +1,6 @@
 class Checkout
   def complete_checkout
-    self.total = self.orders.map{|o| o.products.map(&:price).reduce(&:+)}.reduce(&:+)
-
-    self.card_amount = self.total
-    self.user_funds_amount = 0
-    if self.user.has_customer_funds?
-      self.card_amount = self.total - user.customer_funds
-      self.user_funds_amount = user.customer_funds
-    end
+    calculate_totals
 
     if (authorize_money_from_bank(self.card_amount) &&
         debit_funds_from_user(self.user_funds_amount))
@@ -19,6 +12,17 @@ class Checkout
       MONITOR.increment(:checkout_completed)
     else
       redirect_to_payment_error_page
+    end
+  end
+
+  def calculate_totals
+    self.total = self.orders.map{|o| o.products.map(&:price).reduce(&:+)}.reduce(&:+)
+
+    self.card_amount = self.total
+    self.user_funds_amount = 0
+    if self.user.has_customer_funds?
+      self.card_amount = self.total - user.customer_funds
+      self.user_funds_amount = user.customer_funds
     end
   end
 end
